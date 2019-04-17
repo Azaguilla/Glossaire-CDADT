9fe8c48f1b66fd98acb1a122803252c458eb55d1
Azaguilla <laurie.m1@free.fr>
2019-04-17 09:36:26 +0200

    Correction de l'affichage de la page d'affichage d'une liste de définitions (quand on clique sur un thème)

--- src/app/word/list-word/list-word.component.ts
+++ src/app/word/list-word/list-word.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WordService} from '../../../services/word.service';
 import {ActivatedRoute} from '@angular/router';
 import Word from '../../models/word.model';
@@ -14,9 +14,11 @@ export class ListWordComponent implements OnInit {
   thmTitle: string;
 
   constructor(private wordService: WordService,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    // On récupère et on affiche les données à l'initialisation du composant
     this.route.params.subscribe(params => {
       this.thmTitle = params.title;
       this.wordService.getWordsByThmTitle(params.title).subscribe((data: Word[]) => {
@@ -25,13 +27,46 @@ export class ListWordComponent implements OnInit {
     });
   }
 
+  /**
+   * Effectue des actions d'affichage lorsque l'on clique sur une définition
+   * @param id L'id de la définition
+   */
   onOpenDef(id) {
-    console.log(id);
+
+    // On récupère le contenu et l'icône "flèche"
     const elem = document.getElementById(id);
-    if (elem.className === 'list-word-def-content d-block') {
-      elem.className = 'list-word-def-content d-none';
+    const elemi = document.getElementById('i-' + id);
+
+    // Si le contenu est déjà affiché
+    if (elem.className === 'list-word-def-content block') {
+      // On lui dit de le fermer
+      elem.className = 'list-word-def-content none';
+
+      // La flèche reprend sa position initiale
+      elemi.animate([
+        // keyframes
+        {transform: 'rotate(90deg)'},
+        {transform: 'rotate(0)'}
+      ], {
+        // timing options
+        duration: 250,
+        fill: 'forwards'
+      });
     } else {
-      elem.className = 'list-word-def-content d-block';
+      // Si le contenu est fermé
+      // On lui dit de l'ouvrir
+      elem.className = 'list-word-def-content block';
+
+      // La flèche tourne à 90 degré
+      elemi.animate([
+        // keyframes
+        {transform: 'rotate(0)'},
+        {transform: 'rotate(90deg)'}
+      ], {
+        // timing options
+        duration: 250,
+        fill: 'forwards'
+      });
     }
   }
 }
