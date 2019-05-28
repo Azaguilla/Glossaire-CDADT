1f9161f2fe8bc5ea818363d898fd10561560d1c7
Azaguilla <laurie.m1@free.fr>
2019-05-28 17:14:54 +0200

    --> Corrections apportées suite à la modification des maquettes ajouter le menu burger

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,7 @@ import {WordService} from '../../services/word.service';
 import Word from '../models/word.model';
 import Theme from '../models/theme.model';
 import {ThemeService} from '../../services/theme.service';
+import {AuthenticationService} from '../../services/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -16,12 +17,20 @@ export class HeaderComponent implements OnInit {
   theme: Theme[];
   displayResults: boolean;
 
-  constructor(private wordService: WordService, private themeService: ThemeService) {
+  constructor(private wordService: WordService,
+              private themeService: ThemeService,
+              private authService: AuthenticationService) {
     // L'overlay et le résultat de la recherche ne sont pas affichés par défaut
     this.displayResults = false;
   }
 
   ngOnInit() {
+    // Fixe une taille minimum au menu
+    const winHeight = window.innerHeight;
+    const elem = document.getElementById('header-menu-burger');
+    const header = document.getElementById('header-menu').offsetHeight;
+
+    elem.style.height = `${winHeight - header}px`;
   }
 
   /**
@@ -57,10 +66,93 @@ export class HeaderComponent implements OnInit {
   }
 
 
+  /**
+   * Méthode permettant d'afficher les résultats de la recherche si une recherche est effectuée
+   * si le résultat n'est pas vide
+   */
   onDisplayResult() {
     this.searchValue = (document.getElementById('header-menu-search') as HTMLInputElement).value;
     if (this.searchValue !== null && this.searchValue !== '') {
       this.displayResults = true;
     }
+  }
+
+  /**
+   * Affiche le menu burger
+   */
+  onDisplayMenu() {
+    const elem = document.getElementById('header-menu-burger');
+    const overlay = document.getElementById('overlay-menu');
+
+    if (elem.className === 'header-menu-burger open') {
+      elem.className = 'header-menu-burger';
+
+      overlay.style.display = 'none';
+
+      // On affiche le menu
+      elem.animate([
+        // keyframes
+        {left: '30vw'},
+        {left: '-100%'}
+      ], {
+        // timing options
+        duration: 300,
+        fill: 'forwards'
+      });
+    } else {
+      elem.className = 'header-menu-burger open';
+
+      overlay.style.display = 'block';
+
+      overlay.animate([
+        // keyframes
+        {opacity: '0'},
+        {opacity: '1'}
+      ], {
+        // timing options
+        duration: 150,
+        fill: 'forwards'
+      });
+
+      elem.animate([
+        // keyframes
+        {left: '-100%'},
+        {left: '30vw'}
+      ], {
+        // timing options
+        duration: 300,
+        fill: 'forwards'
+      });
+    }
+  }
+
+  onDisplayMenuNone() {
+    const elem = document.getElementById('header-menu-burger');
+    const overlay = document.getElementById('overlay-menu');
+
+    if (elem.className === 'header-menu-burger open') {
+      elem.className = 'header-menu-burger';
+
+      overlay.style.display = 'none';
+
+      // On affiche le menu
+      elem.animate([
+        // keyframes
+        {left: '30vw'},
+        {left: '-100%'}
+      ], {
+        // timing options
+        duration: 300,
+        fill: 'forwards'
+      });
+    }
+
+  }
+
+  /**
+   * Méthode permettant de savoir si l'utilisateur est connecté ou non
+   */
+  isLoggedIn() {
+    return this.authService.isLoggedIn();
   }
 }
