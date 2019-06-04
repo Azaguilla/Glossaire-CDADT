fc6f0f0e20821c29cac946b795f8e13a1861206d
Azaguilla <laurie.m1@free.fr>
2019-06-04 15:26:10 +0200

    Oubli des fichiers edit-word... :x

--- server/bin/db.js
+++ server/bin/db.js
@@ -1,6 +1,3 @@
 module.exports = {
-  DB: "mongodb://localhost:27017/GlossaireCDADT",
+  DB: "mongodb://azaguilla:lclj83400@mongodb-azaguilla.alwaysdata.net/azaguilla_glossaire",
 };
-// module.exports = {
-//   DB: "mongodb://azaguilla:lclj83400@mongodb-azaguilla.alwaysdata.net/azaguilla_glossaire",
-// };
