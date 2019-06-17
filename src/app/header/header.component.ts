35c3da59f14ac3102fb0c52787fc55cc9eb3c0b6
Azaguilla <laurie.m1@free.fr>
2019-06-17 10:32:24 +0200

    Installation des notifications dans le menu burger

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,10 @@ import Theme from '../models/theme.model';
 import {ThemeService} from '../../services/theme.service';
 import {AuthenticationService} from '../../services/authentication.service';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import {SwPush} from '@angular/service-worker';
+import {NewsletterService} from '../../services/newsletter.service';
+import {TimeagoIntl} from 'ngx-timeago';
+import {strings as FrenchStrings} from 'ngx-timeago/language-strings/fr';
 
 @Component({
   selector: 'app-header',
@@ -50,22 +54,45 @@ export class HeaderComponent implements OnInit {
   isDisplayOverlayMenu: boolean;
   isMenuOpen: boolean;
 
+  isSubscriber: boolean;
+  private subscription;
+  notification: boolean;
+
+  readonly VAPID_PUBLIC_KEY = 'BNGmdT-zn-S0tocFwPP9Z6PG3pfouwebPHQ0lpAQg5Z5LLZJ4OdBXz8aN_ct19Bbvi56WeYosu94RCXS34D2NU0';
+
   constructor(private wordService: WordService,
               private themeService: ThemeService,
-              private authService: AuthenticationService) {
+              private authService: AuthenticationService,
+              private swPush: SwPush,
+              private newsletterService: NewsletterService,) {
     // L'overlay et le résultat de la recherche ne sont pas affichés par défaut
     this.displayResults = false;
     this.isDisplayOverlayMenu = false;
     this.isMenuOpen = false;
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     // Fixe une taille minimum au menu
     const winHeight = window.innerHeight;
     const elem = document.getElementById('header-menu-burger');
     const header = document.getElementById('header-menu').offsetHeight;
 
     elem.style.height = `${winHeight - header}px`;
+
+
+    // vérifie si le navigateur n'est pas Safari, si c'est le cas, vérifie que le navigateur supporte les
+    // notifications et enfin si le navigateur est inscrit aux notifications
+    if (window.navigator.userAgent.indexOf('Safari') > -1 && window.navigator.userAgent.indexOf('Chrome') === -1) {
+      this.notification = false;
+    } else {
+      if (('Notification' in window)) {
+        this.notification = true;
+        await (await navigator.serviceWorker.getRegistration()).pushManager.getSubscription().then(
+          pushSubscription => this.isSubscribe(pushSubscription)).catch(err => console.log(err));
+      } else {
+        this.notification = false;
+      }
+    }
   }
 
   /**
@@ -156,5 +183,69 @@ export class HeaderComponent implements OnInit {
    */
   isLoggedIn() {
     return this.authService.isLoggedIn();
+  }
+
+  // ********************************************************************** NOTIFICATIONS
+  /**
+   * Méthode qui teste si l'abonnement existe ou non. En fonction du résultat, sera affiché un bouton "S'abonner" ou "Se désabonner"
+   * @param pushSubscription Objet subscription
+   */
+  isSubscribe(pushSubscription) {
+    if (pushSubscription === null) {
+      this.isSubscriber = false;
+    } else {
+      this.subscription = pushSubscription.endpoint;
+      this.isSubscriber = true;
+    }
+  }
+
+  /**
+   * Méthode appelée lorsque l'utilisateur clique sur le bouton "S'abonner aux notifications"
+   * Demande au service web push d'inscrire la personne aux notification en générant une subscription "sub"
+   */
+  subscribeToNotifications() {
+
+    // On inscrit la personne
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    }).then(
+      sub => this.subscriptionSuccessful(sub)
+      , err => console.log(err)
+    );
+  }
+
+  /**
+   * Méthode permettant de désinscrire le naviagteur aux notifications. Puis on utilise la fonction unsubscriptionSuccessful
+   * pour supprimer l'entrée concernant l'abonnement dans la Base de Données
+   */
+  async unsubscribeToNotifications() {
+
+    // On désinscrit la personne
+    await (await navigator.serviceWorker.getRegistration()).pushManager.getSubscription().then(
+      pushSubscription => pushSubscription.unsubscribe()).then(
+      success => this.unsubscriptionSuccessful()
+    );
+  }
+
+  /**
+   * Méthode permettant d'afficher le message de succès lors d'une tentative d'abonnement qui aurait réussi
+   * Puis envoie la subscription pour enregistrement dans la Base de données
+   * @param sub L'objet subscription
+   */
+  subscriptionSuccessful(sub) {
+
+    // On indique à la page que la personne s'est abonnée et on lui renseigne le endpoint si jamais la personne souhaite
+    // se désabonner. Puis on l'inscrit dans la Base de Données
+    this.isSubscriber = true;
+    this.subscription = sub.endpoint;
+    this.newsletterService.addPushSubscriber(sub).subscribe();
+  }
+
+  /**
+   * Méthode permettant de supprimer l'entrée d'une personne abonnée dans la Base de Données
+   */
+  unsubscriptionSuccessful() {
+    this.isSubscriber = false;
+    this.newsletterService.deletePushSubscriber(this.subscription).subscribe();
   }
 }
